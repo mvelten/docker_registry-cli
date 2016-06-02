59acc1c9e0bcbfc1b059e549b559495229844587
Mathias Velten <mathias.velten@flavia-it.de>
2016-06-02 18:15:23 +0200

    added argpars cli parameters

--- registry-cli.py
+++ registry-cli.py
@@ -1,6 +1,6 @@
+import argparse
 import json
 import requests
-import sys
 
 
 def get_all_repos(url):
@@ -36,7 +36,7 @@ def print_all_blob_sizes(url, repo, tag):
     blob_counter = 0
 
     for blob in parsed_json['fsLayers']:
-        blob_size_in_kilobyte = float(get_blob_size(regurl, repo_to_list, blob['blobSum'])) / 1000
+        blob_size_in_kilobyte = float(get_blob_size(url, repo, blob['blobSum'])) / 1000
         id = parsed_json['history'][blob_counter]["v1Compatibility"]
         v1_compat_json = json.loads(parsed_json['history'][blob_counter]["v1Compatibility"])
         id = v1_compat_json["id"]
@@ -45,48 +45,68 @@ def print_all_blob_sizes(url, repo, tag):
         blob_counter += 1
 
 
-if __name__ == "__main__":
-    # TODO add usage + parameter check
-    regurl = sys.argv[1:][0]
-    command = sys.argv[1:][1]
+def delete_digest(url, repo, digest):
+    response = requests.delete(url + "/v2/" + repo + "/manifests/" + digest)
+    return response.status_code
 
-    if command == "list":
-        repo_to_list = sys.argv[1:][2]
-        if repo_to_list == "all":
-            try:
-                all_repos = get_all_repos(regurl)
-                for repo_in_registry in all_repos:
-                    print()
-            except:
-                print("no repos found")
-                exit(1)
-
-        else:
-            try:
-                all_tags = get_all_tags_for_repo(regurl, repo_to_list)
-            except:
-                print("no tags for this repo")
-                exit(1)
-            for tag_in_repo in all_tags:
-                print(tag_in_repo)
-
-    elif command == "delete":
-        image_to_delete = sys.argv[1:][2]
-        tag_to_delete = sys.argv[1:][3]
+#
+# base functions
+#
+def list(args):
+    repo_to_list = format(args.repo)
+    if repo_to_list == "all":
         try:
-            digest = get_digest(regurl, image_to_delete, tag_to_delete)
+            all_repos = get_all_repos(format(args.regurl))
+            for repo_in_registry in all_repos:
+                print(repo_in_registry)
         except:
-            print("no digest found for tag")
+            print("no repos found")
             exit(1)
-        try:
-            response = requests.delete(regurl + "/v2/" + image_to_delete + "/manifests/" + digest)
-        except:
-            print("Error deleting image")
-
-    elif command == "show-blobsize":
-        repo_to_list = sys.argv[1:][2]
-        tag_in_repo = sys.argv[1:][3]
-        parsed_json = print_all_blob_sizes(regurl, repo_to_list, tag_in_repo)
-
     else:
-        print("foo")
+        response = get_all_tags_for_repo(format(args.regurl), format(args.repo))
+        for tag in response:
+            print(tag)
+
+def show_blobsize(args):
+    parsed_json = print_all_blob_sizes(format(args.regurl), format(args.repo), format(args.tag))
+
+def delete(args):
+    try:
+        digest = get_digest(format(args.regurl), format(args.repo), format(args.tag))
+    except:
+        print("no digest found for tag")
+        exit(1)
+    try:
+        delete_digest(format(args.regurl), format(args.repo), digest)
+    except:
+        print("Error deleting image")
+        exit(1)
+#
+# argparse initialization
+#
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+list_parser = subparsers.add_parser('list')
+list_parser.add_argument('repo')
+list_parser.add_argument('regurl')
+list_parser.set_defaults(func=list)
+
+delete_parser = subparsers.add_parser('delete')
+delete_parser.add_argument('repo')
+delete_parser.add_argument('tag')
+delete_parser.add_argument('regurl')
+delete_parser.set_defaults(func=delete)
+
+show_blobsize_parser = subparsers.add_parser('show-blobsize')
+show_blobsize_parser.add_argument('repo')
+show_blobsize_parser.add_argument('tag')
+show_blobsize_parser.add_argument('regurl')
+show_blobsize_parser.set_defaults(func=show_blobsize)
+
+#
+# main function
+#
+if __name__ == '__main__':
+    args = parser.parse_args()
+    args.func(args)  # call the default function
