b7bd9569f1c60dd0034d92abfe302b67e1ab93cd
Mathias Velten <mvelten@users.noreply.github.com>
2016-06-02 14:20:57 +0200

    added basic error handling

--- registry-cli.py
+++ registry-cli.py
@@ -24,20 +24,38 @@ def get_digest(regurl, repo, tag):
 
 
 if __name__ == "__main__":
+    #TODO add usage + parameter check
     regurl = sys.argv[1:][0]
-    keyword = sys.argv[1:][1]
+    command = sys.argv[1:][1]
 
-    if keyword == "list":
+    if command == "list":
         repo_to_list = sys.argv[1:][2]
         if repo_to_list == "all":
-            for repo_in_registry in get_all_repos(regurl):
+            try:
+                all_repos = get_all_repos(regurl)
+                for repo_in_registry in all_repos:
                     print repo_in_registry
+            except:
+                print "no repos found"
+                exit(1)
         else:
-            for tag_in_repo in get_all_tags_for_repo(regurl, repo_to_list):
+            try:
+                all_tags = get_all_tags_for_repo(regurl, repo_to_list)
+            except:
+                print "no tags for this repo"
+                exit(1)
+            for tag_in_repo in all_tags:
                 print tag_in_repo
 
-    elif keyword == "delete":
+    elif command == "delete":
         image_to_delete = sys.argv[1:][2]
         tag_to_delete = sys.argv[1:][3]
-        digest = get_digest(regurl, image_to_delete, tag_to_delete)
-        response = requests.delete(regurl + "/v2/" + image_to_delete + "/manifests/" + digest)
+        try:
+            digest = get_digest(regurl, image_to_delete, tag_to_delete)
+        except:
+            print "no digest found for tag"
+            exit(1)
+        try:
+            response = requests.delete(regurl + "/v2/" + image_to_delete + "/manifests/" + digest)
+        except:
+            print "Error deleting image"
